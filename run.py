3b5e8ee1bdb51819fa2bff5175997431cfa93159
Drlz0 <vinidomluk@gmail.com>
2023-04-18 20:40:56 +0200

    it's stupid approach with this bot.py

--- run.py
+++ run.py
@@ -1,10 +1,8 @@
 from base.bot import Bot
 
-
-with Bot(teardown=True) as bot:
-    bot.open_home_page()
-    bot.search_for_product(query='fantasy book')
-    bot.filter_search_results()  # Defining specific filters in filtration() function in ebay_main.py
-    bot.scrape_results(amount=10)  # Define specific amount of displayed results
-                                   # in scrap_results() function in ebay_main.py
-
+bot = Bot()
+bot.choose_category('Books')
+bot.search_for_product('Python')
+bot.filter_search_results()
+bot.scrape_results(amount=5)
+bot.__exit__(None, None, None)
